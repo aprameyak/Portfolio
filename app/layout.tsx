80613280733bbdcd99ab04f132f9d276bbb66b06
Aprameya Kannan <aprameyakannan@gmail.com>
2025-08-12 01:26:04 -0400

    fix'

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
   },
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className={`scroll-smooth ${inter.variable}`}>
       <body className={`${inter.className} antialiased bg-background text-text`}>
