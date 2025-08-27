85f5a313787726a6ee588d55b1fcdc86cdf1f39c
Aprameya Kannan <aprameyakannan@gmail.com>
2025-08-27 01:47:14 -0400

    fixed

--- app/components/Hero3D.tsx
+++ app/components/Hero3D.tsx
@@ -7,7 +7,7 @@ const Hero3D = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const particlesRef = useRef<Particle[]>([]);
   const mouseRef = useRef({ x: 0, y: 0 });
-  const animationFrameRef = useRef<number>();
+  const animationFrameRef = useRef<number>(0);
   const timeRef = useRef(0);
 
   interface Particle {
